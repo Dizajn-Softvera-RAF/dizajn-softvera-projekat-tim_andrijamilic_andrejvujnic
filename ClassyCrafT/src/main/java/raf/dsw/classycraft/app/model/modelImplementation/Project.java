17e468fa7e9d471683a70bd7e729771cde1b5bd4
andrejvujnic <avujnic9523rn@raf.rs>
2023-11-08 20:54:52 +0100

    hhh

--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/model/modelImplementation/Project.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/model/modelImplementation/Project.java
@@ -5,6 +5,8 @@ import raf.dsw.classycraft.app.model.repositoryModel.ClassyNode;
 import raf.dsw.classycraft.app.model.repositoryModel.ClassyNodeComposite;
 
 public class Project extends ClassyNodeComposite {
+    private String autor;
+
     public Project(String name, ClassyNode parent) {
         super(name, parent);
     }
