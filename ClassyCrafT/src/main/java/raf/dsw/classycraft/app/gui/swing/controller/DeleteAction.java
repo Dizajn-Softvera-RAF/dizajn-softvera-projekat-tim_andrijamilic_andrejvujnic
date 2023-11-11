99f17e74121f0266bfc01c69e1ec6b856287ad99
andrejvujnic <avujnic9523rn@raf.rs>
2023-11-11 13:50:26 +0100

    Delete

--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/gui/swing/controller/DeleteAction.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/gui/swing/controller/DeleteAction.java
@@ -1,8 +1,18 @@
 package raf.dsw.classycraft.app.gui.swing.controller;
 
+import raf.dsw.classycraft.app.core.ApplicationFramework;
+import raf.dsw.classycraft.app.gui.swing.tree.model.ClassyTreeItem;
+import raf.dsw.classycraft.app.gui.swing.view.MainFrame;
+import raf.dsw.classycraft.app.messanger.MessageType;
+import raf.dsw.classycraft.app.model.modelImplementation.ProjectExplorer;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 
 public class DeleteAction extends AbstractClassyAction{
     public DeleteAction(){
@@ -13,6 +23,18 @@ public class DeleteAction extends AbstractClassyAction{
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        ClassyTreeItem selected = (ClassyTreeItem) MainFrame.getInstance().getClassyTree().getSelectedNode();
+
+        if(selected == null){
+            System.out.println("Nije dobro1");
+        }else if((selected.getClassyNode() instanceof ProjectExplorer)){
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            LocalDateTime ldt = LocalDateTime.now();
+            ApplicationFramework.getInstance().getMessageGenerator().generateMessage("Poruka", MessageType.WARNING);
+        }else{
+            MainFrame.getInstance().getClassyTree().deleteNode(selected);
+        }
+
 
     }
 
