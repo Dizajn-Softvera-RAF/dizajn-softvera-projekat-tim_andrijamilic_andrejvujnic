0c17fa39ec701fb06bd879e090931aa31d845a2a
andrejvujnic <avujnic9523rn@raf.rs>
2023-11-12 19:07:00 +0100

    Bravo

--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainFrame.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainFrame.java
@@ -51,8 +51,19 @@ public class MainFrame extends JFrame {
         JPanel desktop=new JPanel();
         desktop.setBackground(Color.WHITE);
 
-        /*JPanel left=new JPanel();
-        left.setBackground(Color.WHITE);*/
+        /*JTabbedPane tp = new JTabbedPane();
+        tp = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
+
+
+        tp.addTab("Listing", new JPanel());
+        tp.addTab("Comment", new JTextArea(10, 40));
+
+        tp.addTab("Register", new JPanel());
+        tp.addTab("Contact Us", new JPanel());
+
+        tp.addTab("More..", new JPanel());
+
+        desktop.add(tp);*/
 
         JTree projectExplorer = classyTree.generateTree(ApplicationFramework.getInstance().getClassyRepository().getProjectExplorer());
 
