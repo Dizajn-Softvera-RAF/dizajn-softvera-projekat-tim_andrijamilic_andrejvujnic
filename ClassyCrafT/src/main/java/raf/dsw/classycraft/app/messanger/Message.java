4a3bccd0e6cfef6882d7819f24750c5dffeaa1bc
andrejvujnic <avujnic9523rn@raf.rs>
2023-11-14 11:37:27 +0100

    Bravo

--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/messanger/Message.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/messanger/Message.java
@@ -1,11 +1,14 @@
 package raf.dsw.classycraft.app.messanger;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.lang.reflect.Type;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-
+@Getter
+@Setter
 public class Message {
     private MessageType type;
     private String tekst;
